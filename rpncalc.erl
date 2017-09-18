d32068bdfc45a42977fe1884425145201fc99b3a
Yazan Lpizra <ylpizra@my.bridgeport.edu>
2017-09-17 20:35:31 -0400

    removed all the *.beam files.

--- rpncalc.erl
+++ rpncalc.erl
@@ -2,25 +2,30 @@
 -export([evaluate/1]).
 
 evaluate(Exp) ->
-    evaluate(Exp, []).
+    evaluate(string:tokens(Exp," "), []).
 
-evaluate([], [X]) ->
+evaluate(_, [X]) ->
     X;
-evaluate([H|T], Stack)->
-    try
-        string:to_float(H)
-    of
-        {Num, _} -> 
-            evaluate(T, [Num|Stack])
-    catch
-        {error, _} -> 
-            X = H,
-            Y = hd(Stack),
-            NewT = tl(Stack),
-            case H of
-                "+" -> evaluate(T, [X+Y|NewT]);
-                "-" -> evaluate(T, [X-Y|NewT]);
-                "*" -> evaluate(T, [X*Y|NewT]);
-                "/" -> evaluate(T, [X/Y|NewT])
-            end
+evaluate([H|T], Stack) when string:to_integer(H) =/= {error,_} ->
+    io:format("Head: ~s~n",[H]),
+    io:format("Tail: ~p~n",[T]),
+    io:format("Stack: ~p~n",[Stack]),
+    evaluate(T, [list_to_integer(H)|Stack]);
+evaluate([H|T], Stack) when string:to_float(H) =/= {error,_} ->
+    io:format("Head: ~s~n",[H]),
+    io:format("Tail: ~p~n",[T]),
+    io:format("Stack: ~p~n",[Stack]),
+    evaluate(T, [list_to_float(H)|Stack]);
+evaluate([H|T], Stack) ->
+    io:format("Head: ~s~n",[H]),
+    io:format("Tail: ~p~n",[T]),
+    io:format("Stack: ~p~n",[Stack]),
+    X = H,
+    Y = hd(Stack),
+    NewT = tl(Stack),
+    case H of
+        "+" -> evaluate(T, [(X+Y)|NewT]);
+        "-" -> evaluate(T, [(X-Y)|NewT]);
+        "*" -> evaluate(T, [(X*Y)|NewT]);
+        "/" -> evaluate(T, [(X/Y)|NewT])
     end.
